48eae0e331038f2eafb3f97cbbd7bfe0a915ae5a
ariadnimac <ariadnimak@yahoo.com>
2020-04-18 00:59:40 +0300

    add testing class ArithmeticOperationsTest

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -4,10 +4,14 @@ public class MyMath {
 
 	public long factorial (int n) {
 		long result = 1;
+		if((0<n) & (n>20)){
+			for (int factor = 2; factor <= n; factor++) {
+				result *= factor;
+			}
+		}else {
+			throw new IllegalArgumentException("Given number must be between 0 and 20");
 
-        for (int factor = 2; factor <= n; factor++) {
-            result *= factor;
-        }
+		}
 
         return result;
     }
