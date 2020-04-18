daac53cdee6987739ea6a97beb25b1f83003736a
ariadnimac <ariadnimak@yahoo.com>
2020-04-18 17:02:36 +0300

    add tests for divide method

--- seip2020_practical_assignments/unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -1,5 +1,24 @@
 package math;
+import org.junit.Test;
+
+import math.ArithmeticOperations;
+
+import org.junit.Assert;
 
 public class ArithmeticOperationsTest {
+	ArithmeticOperations ao = new ArithmeticOperations();
+	
+	@Test (expected = ArithmeticException.class)
+	public void test_divide_zero_denominator_exception() {
+		ao.divide(2, 0);
+	}
+	
+
+	@Test 
+	public void test_divide_valid_denominator() {
+		Assert.assertEquals(4.2, ao.divide(21.42, 5.1),0.00001);
+	}
+	
+
 
 }
