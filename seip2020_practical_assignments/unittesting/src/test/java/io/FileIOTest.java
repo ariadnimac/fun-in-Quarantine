f189609311a8d7ba2a997b76d1e306309934ecc9
ariadnimac <ariadnimak@yahoo.com>
2020-04-20 23:56:07 +0300

    add test class FileIOTest for FileIO

---
+++ seip2020_practical_assignments/unittesting/src/test/java/io/FileIOTest.java
@@ -0,0 +1,11 @@
+package io;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class FileIOTest {
+	FileIO fileio = new FileIO();
+	public static String resourcesPath = "src/test/resources/";
+	
+}
