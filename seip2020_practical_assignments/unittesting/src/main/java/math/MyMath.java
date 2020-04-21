4fa929eefacc3439d0f058834bca69582b0e75b3
ariadnimac <ariadnimak@yahoo.com>
2020-04-21 13:29:36 +0300

    add isPrime method in MyMath class

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -16,4 +16,16 @@ public class MyMath {
         return result;
     }
 	
+	public boolean isPrime(int n) {
+		if (n<2) {
+			throw new IllegalArgumentException("Given number must be bigger than 2");
+
+		}else {
+			for (int i = 2; i < n; i++) 
+				if (n % i == 0) 
+					return false; 
+		}
+		return true;
+	}
+	
 }
