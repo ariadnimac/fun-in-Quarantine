48131d8f60986382ce96705817c5d481fe9eed56
ariadnimac <ariadnimak@yahoo.com>
2020-05-30 11:07:29 +0300

    change src analyzer design -->Strategy Pattern

---
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/SrcAnalyzer.java
@@ -0,0 +1,15 @@
+package codeanalyzer;
+
+import java.io.IOException;
+
+public interface SrcAnalyzer {
+	
+	public int calculateNOM(String filepath, String analyzerType) throws IOException;
+	public int calculateLOC(String filepath, String analyzerType) throws IOException;
+	public int calculateNOC(String filepath, String analyzerType) throws IOException;
+	
+	
+
+		
+
+}
---
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/StrcompAnalyzer.java
@@ -0,0 +1,53 @@
+package codeanalyzer;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class StrcompAnalyzer implements SrcAnalyzer{
+private SourceFileReader fileReader;
+	
+	public StrcompAnalyzer(String fileReaderType) {
+		this.fileReader = new SourceFileReader(fileReaderType);
+	}
+	
+	public int calculateNOM(String filepath, String analyzerType) throws IOException {
+		List<String> sourceCodeList = fileReader.readFileIntoList(filepath);
+		int methodCounter = 0;
+		for (String line : sourceCodeList) {
+			line = line.trim().trim(); //clear all leading and trailing white spaces
+			if ( ((line.contains("public") || line.contains("private") || line.contains("protected"))
+					|| line.contains("void") || line.contains("int") || line.contains("String"))
+					&& line.contains("(") && line.contains(")") && line.contains("{") )
+					methodCounter++;
+		}
+		return methodCounter; 
+	}
+	
+	public int calculateLOC(String filepath, String analyzerType) throws IOException{
+		List<String> sourceCodeList = fileReader.readFileIntoList(filepath);
+		int nonCodeLinesCounter = 0;
+		for (String line : sourceCodeList) {
+			line = line.trim().trim(); //clear all leading and trailing white spaces
+			if (line.startsWith("//") || line.startsWith("/*") || line.startsWith("*") || line.equals("{") || line.equals("}") || line.equals(""))
+				nonCodeLinesCounter++;
+		}
+		int loc = sourceCodeList.size() - nonCodeLinesCounter;
+		return loc; 
+		
+	}
+	
+	public int calculateNOC(String filepath, String analyzerType) throws IOException{
+		List<String> sourceCodeList = fileReader.readFileIntoList(filepath);
+		int classCounter = 0;
+		for (String line : sourceCodeList) {
+			line = line.trim(); //remove leading and trailing white spaces
+			if ((line.startsWith("class ") || line.contains(" class ")) && line.contains("{")) {
+				classCounter++;
+			}
+		}
+		return classCounter; 
+		
+	}
+
+}
