4854a58ea4e18eb5c73b0c690ff778178c25b2f2
ariadnimac <ariadnimak@yahoo.com>
2020-05-30 17:54:05 +0300

    add methods to local and web reader classes

--- seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/WebReader.java
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/WebReader.java
@@ -1,5 +1,45 @@
 package codeanalyzer;
 
-public class WebReader {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebReader implements SrcFileReader{
+	private String type;
+	
+	public WebReader(String _type) {
+		this.type = _type;
+	}
+
+	
+	@Override
+	public String readFileIntoString(String filepath) throws IOException{
+		StringBuilder sb = new StringBuilder();
+        URL url = new URL(filepath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+        	sb.append(line + "\n");
+        }
+        reader.close();
+		return sb.toString();
+	}
+	
+	@Override
+	public List<String> readFileIntoList(String filepath) throws IOException{
+		List<String> lines = new ArrayList<>();
+        URL url = new URL(filepath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+        	lines.add(line);
+        }
+        reader.close();
+		return lines;
+		
+	}
 
 }
